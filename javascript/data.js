b8e1a4aa1d59ff50f8a53e05d3c6cd08daf7e8c8
ysilkov <ysilkov@ukr.net>
2022-09-12 18:01:44 +0300

    update

--- javascript/data.js
+++ javascript/data.js
@@ -1,56 +1,56 @@
-import { ideaLogo , quoteLogo, taskLogo, thoughtLogo } from "./logo.js"
+import { ideaLogo, quoteLogo, taskLogo, thoughtLogo } from "./logo.js";
 
 export let categories = {
-    "Idea": ideaLogo,
-    "Quote": quoteLogo,
-    "Task": taskLogo,
-    "Random Thought": thoughtLogo
-}
+  Idea: ideaLogo,
+  Quote: quoteLogo,
+  Task: taskLogo,
+  "Random Thought": thoughtLogo,
+};
 
 export let notes = [
-    {
-        id: "1",
-        name: "Shopping list",
-        created: "April 20, 2021",
-        category: "Task",
-        content: "Tomatoes, bread",
-        dates: "",
-        archived: false
-    },
-    {
-        id: "2",
-        name: "The theory of evolution",
-        created: "April 27, 2021",
-        category: "Random Thought",
-        content: "The evolution is ..........",
-        dates: "",
-        archived: false
-    },
-    {
-        id: "3",
-        name: "New Feature",
-        created: "May 05, 2021",
-        category: "Idea",
-        content: "Implement new applwcation on the 3/5/2021, show it from 5/5/2021",
-        dates: "3/5/2021, 5/5/2021",
-        archived: false
-    },
-    {
-        id: "4",
-        name: "William Gaddis",
-        created: "May 07, 2021",
-        category: "Quote",
-        content: "Power doesn't have start",
-        dates: "",
-        archived: true
-    },
-    {
-        id: "5",
-        name: "Books",
-        created: "May 15, 2021",
-        category: "Task",
-        content: "The Lean Statrup",
-        dates: "",
-        archived: true
-    },
-]
+  {
+    id: "1",
+    name: "Shopping list",
+    created: "April 20, 2021",
+    category: "Task",
+    content: "Tomatoes, bread",
+    dates: "",
+    archived: false,
+  },
+  {
+    id: "2",
+    name: "The theory of evolution",
+    created: "April 27, 2021",
+    category: "Random Thought",
+    content: "The evolution is ..........",
+    dates: "",
+    archived: false,
+  },
+  {
+    id: "3",
+    name: "New Feature",
+    created: "May 05, 2021",
+    category: "Idea",
+    content: "Implement new applwcation on the 3/5/2021, show it from 5/5/2021",
+    dates: "3/5/2021, 5/5/2021",
+    archived: false,
+  },
+  {
+    id: "4",
+    name: "William Gaddis",
+    created: "May 07, 2021",
+    category: "Quote",
+    content: "Power doesn't have start",
+    dates: "",
+    archived: true,
+  },
+  {
+    id: "5",
+    name: "Books",
+    created: "May 15, 2021",
+    category: "Task",
+    content: "The Lean Statrup",
+    dates: "",
+    archived: true,
+  },
+];
--- javascript/index.js
+++ javascript/index.js
@@ -1,79 +1,91 @@
-import { notes, categories } from './data.js';
-import { createNote, updateNote, deleteNote, changeArchiveState } from './helperNote.js';
-import { archiveLogo, deleteLogo, editLogo, unArchiveLogo } from './logo.js';
-/* import { makeRandomID, getDatesFromText } from "./randomID.js"; */
+import { categories } from "./data.js";
+import { createNote, updateNote } from "./helperNote.js";
+import { archiveLogo, deleteLogo } from "./logo.js";
 
-
-
-//svg icons to styled
-export const loadIconsIntoHeader = ()=> {
-    Array.from(document.getElementsByClassName('header-icon')).forEach(col => {
-        if (col.classList.contains('archive'))
-            col.innerHTML = archiveLogo;
-        if (col.classList.contains('delete'))
-            col.innerHTML = deleteLogo;
-    });
-}
-
-//Note
-
-//announcer
+export const loadIconsIntoHeader = () => {
+  Array.from(document.getElementsByClassName("header-icon")).forEach((col) => {
+    if (col.classList.contains("archive")) col.innerHTML = archiveLogo;
+    if (col.classList.contains("delete")) col.innerHTML = deleteLogo;
+  });
+};
 export const showAnnouncer = (text, error) => {
-    let announcer = document.getElementById('announcer')
-    announcer.style.opacity = '1';
-    announcer.innerText = text;
-    if (error)
-        announcer.classList.add('invalid-input');
-    setTimeout(() => {
-        announcer.style.opacity = '0';
-        if (error)
-            announcer.classList.remove('invalid-input');
-    }, 1500);
-}
+  let announcer = document.getElementById("announcer");
+  announcer.style.opacity = "1";
+  announcer.innerText = text;
+  if (error) announcer.classList.add("invalid-input");
+  setTimeout(() => {
+    announcer.style.opacity = "0";
+    if (error) announcer.classList.remove("invalid-input");
+  }, 1500);
+};
 
 export const buildForm = (note) => {
-    let form = document.createElement('form');
+  let form = document.createElement("form");
 
-    form.innerHTML = `
-        <input type="text" name="name" value="${typeof note.name === "string" ? note.name : ''}" placeholder="Name" required>
+  form.innerHTML =
+    `
+        <input type="text" name="name" value="${
+          typeof note.name === "string" ? note.name : ""
+        }" placeholder="Name" required>
         <select name="categories">
-        ` + Object.keys(categories).map(elem => `<option value="${elem}" ${note.category === elem ? 'selected' : ''}>${elem}</option>`) + `
+        ` +
+    Object.keys(categories).map(
+      (elem) =>
+        `<option value="${elem}" ${
+          note.category === elem ? "selected" : ""
+        }>${elem}</option>`
+    ) +
+    `
         </select>
-        <textarea name="content" placeholder="Content">${note.content ? note.content : ''}</textarea>
+        <textarea name="content" placeholder="Content">${
+          note.content ? note.content : ""
+        }</textarea>
         <input class="cancel" type="button" value="Cancel">
         <input id="submit-button" type="submit" value="Submit" > 
     `;
-    form.getElementsByClassName('cancel')[0].addEventListener("click", () => {
-        document.getElementsByClassName('wrapper-div')[0].remove();
-    });
-const month = ["January", "February","March", "April", "May", "June", "July", "August", "September", "October", 
-"November", "December"]
-    form.onsubmit = (event) => {
-        event.preventDefault();
-        let newNote = {
-            id: (typeof note.name === "string") ? note.id : new Date().getTime(),
-            name: event.target.name.value,
-            created: (typeof note.name === "string") ? note.created : `${month[new Date().getMonth()]} ${new Date().getDate()}, ${new Date().getFullYear()}`,
-            category: event.target.categories.value,
-            content: event.target.content.value,
-            dates: "",
-            archived: (typeof note.name === "string") ? note.archived : false
-        }
-
-        if (typeof note.name === "string")
-            updateNote(newNote);
-        else
-            createNote(newNote);
-
-        document.getElementsByClassName('wrapper-div')[0].remove();
+  form.getElementsByClassName("cancel")[0].addEventListener("click", () => {
+    document.getElementsByClassName("wrapper-div")[0].remove();
+  });
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  form.onsubmit = (event) => {
+    event.preventDefault();
+    let newNote = {
+      id: typeof note.name === "string" ? note.id : new Date().getTime(),
+      name: event.target.name.value,
+      created:
+        typeof note.name === "string"
+          ? note.created
+          : `${
+              month[new Date().getMonth()]
+            } ${new Date().getDate()}, ${new Date().getFullYear()}`,
+      category: event.target.categories.value,
+      content: event.target.content.value,
+      dates: "",
+      archived: typeof note.name === "string" ? note.archived : false,
     };
 
+    if (typeof note.name === "string") updateNote(newNote);
+    else createNote(newNote);
 
-    let wrapperDiv = document.createElement('div');
-    wrapperDiv.className = 'wrapper-div';
-    wrapperDiv.append(form);
+    document.getElementsByClassName("wrapper-div")[0].remove();
+  };
 
-    document.body.prepend(wrapperDiv);
-}
+  let wrapperDiv = document.createElement("div");
+  wrapperDiv.className = "wrapper-div";
+  wrapperDiv.append(form);
 
-//table
+  document.body.prepend(wrapperDiv);
+};
